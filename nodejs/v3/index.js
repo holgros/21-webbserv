93724a5e9af677fe85b0a881c5e1bcd76392ac3b
Elev <holgros@gmail.com>
2022-01-17 09:21:31 +0100

    Fortsättning NodeJS

---
+++ nodejs/v3/index.js
@@ -0,0 +1,9 @@
+// express är ett externt paket som möjliggör att köra webbserver med JavaScript
+const express = require("express"); // importera express (kräver att man först kör "npm install express" i kommandotolken)
+const app = express();              // skapa en server
+app.get("/", function(req, res) {   // lyssna på GET-anrop på routen "/", dvs. serverns rotadress
+   //res.send("Hejsan!");            // skriv ett enkelt textmeddelande till klienten
+   res.sendFile(__dirname + "/exempel.html");   // visa en webbsida för klienten
+});
+app.listen(3000);   // kör servern på porten 3000
+console.log("Kör server på localhost:3000");
