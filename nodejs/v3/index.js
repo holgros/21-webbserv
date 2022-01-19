4065bfa0c464bfa11336e4c1d80c8db8012bd545
Elev <holgros@gmail.com>
2022-01-19 12:05:39 +0100

    Serva statiska resurser med Express NodeJS-server

--- nodejs/v3/index.js
+++ nodejs/v3/index.js
@@ -7,3 +7,6 @@ app.get("/", function(req, res) {   // lyssna på GET-anrop på routen "/", dvs.
 });
 app.listen(3000);   // kör servern på porten 3000
 console.log("Kör server på localhost:3000");
+
+// För att servern ska kunna hitta filer som bildfiler så måste man serva en publik mapp med statiska resurser
+app.use(express.static("publik"));  // gör all filer i mappen "publik" tillgängliga
