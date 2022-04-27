a5a3263a720d6ef0ae607b43204fd6880b1e6d18
Elev <holgros@gmail.com>
2022-04-27 12:13:03 +0200

    Säkerhet med token del 2

--- nodejs/v17/index.js
+++ nodejs/v17/index.js
@@ -40,7 +40,8 @@ app.post("/login", function(req, res) {
             //Denna kod skapar en token att returnera till anroparen.
             let payload = {
                 sub: result[0].userId,      //sub är obligatorisk
-                name: result[0].firstname   //Valbar information om användaren
+                name: result[0].firstname,   //Valbar information om användaren
+                lastname: result[0].lastname
             }
             let token = jwt.sign(payload, "EnHemlighetSomIngenKanGissaXyz123%&/");
             res.json(token);
@@ -51,4 +52,27 @@ app.post("/login", function(req, res) {
     });
 });
 
-   
+app.get("/users", function(req, res) {
+    let authHeader = req.headers['authorization']
+    if (authHeader === undefined) {
+        // skicka lämplig HTTP-status om auth-header saknas, en “400 någonting”
+        res.sendStatus(400);    // "Bad request"
+        return;
+    }
+    let token = authHeader.slice(7) // tar bort "BEARER " från headern.
+    // nu finns den inskickade token i variabeln token
+    console.log(token);
+    
+    // avkoda token
+    let decoded
+    try {
+        decoded = jwt.verify(token, "EnHemlighetSomIngenKanGissaXyz123%&/")
+    } catch (err) {
+        console.log(err) //Logga felet, för felsökning på servern.
+        res.status(401).send("Invalid auth token")
+    }
+    
+    //Gör något bra med decoded.
+    res.send("Tjena " + decoded.name + " " + decoded.lastname);
+    
+});
